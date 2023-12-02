c005782e936ee62d3d78273bb96d1100d10acbf4
Ryan Novitski <rnovitsk@bowdoin.edu>
2023-12-02 00:02:50 -0500

    Added bash files to start server via IPList.txt

--- indivDroneSetup.sh
+++ indivDroneSetup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+# This bash file is called on each server to initialize or join network
+# THIS IS A HELPER BASH FILE CALLED BY anthillSetup.sh
+
+# Inputs given from anthillSetup.sh
 BOOTSTRAP_IP=$1
 ip_address=$2
 
-cd p4-final-r-2 
-git pull
-make
-
-    if [ -z "$BOOTSTRAP_IP" ]; then # First IP in list is initializer
+if [ -z "$BOOTSTRAP_IP" ]; then # First IP in list is initializer
         BOOTSTRAP_IP=$ip_address
         java anthill.Drone --initialize # runs initializeNetwork()
         
@@ -16,5 +16,4 @@ make
         # if bootstrap is already set, join network
         java anthill.Drone --join $BOOTSTRAP_IP # runs joinNetwork(bootstrapIP)
         
-    fi
-done
+fi

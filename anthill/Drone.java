262bb2fb56e0d38984e8bdd5ea8facb747b24769
Ryan Novitski <rnovitsk@bowdoin.edu>
2023-12-03 21:18:24 -0500

    Fixed implementation of ping

--- anthill/Drone.java
+++ anthill/Drone.java
@@ -321,7 +321,7 @@ public class Drone {
             } else if ("--join".equals(args[0]) && args.length > 1) {
                 ant.joinNetwork(args[1]); // assuming the second argument is the IP address to join
             } else if ("--ping".equals(args[0])) {
-                return true;
+                ant.ping();
             }
         }
         debug = true;

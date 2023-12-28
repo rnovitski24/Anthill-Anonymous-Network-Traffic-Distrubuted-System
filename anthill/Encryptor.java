fe1b68cb7c60bab13d371982973bf9adfdac95c5
Noirex <rawsthorne.grundy@gmail.com>
2023-12-28 01:14:13 -0600

    Pruned Shell Scripts and Added Encryption Code

--- anthill/Encryptor.java
+++ anthill/Encryptor.java
@@ -1,6 +1,9 @@
 package anthill;
 import java.security.*;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 
 // Global/Instance vars HERE
@@ -10,10 +13,39 @@ import java.util.HashMap;
  */
 public class Encryptor {
 
-    //protected HashMap<String, String>
+    SecureRandom secRand = new SecureRandom();
+    private final HashMap<byte[], Queue<String>> pathStorage = new HashMap<>();
 
-    public Encryptor(){
+    public Encryptor() {
     }
-    // default constructor
+    public byte[] getNewPathId(){
+        byte[] randBytes = new byte[256];
+        secRand.nextBytes(randBytes);
+        return randBytes;
+    }
+
+    protected boolean storePath(byte[] hash, String successor){
+        //because nodes can be visited multiple times, multiples are stored Fifo in queue.
+        if(pathStorage.containsKey(hash)){
+            pathStorage.get(hash).add(successor);
+        }
+        else {
+            Queue<String> tempStack = new LinkedList<>();
+            tempStack.add(successor);
+            pathStorage.put(hash, tempStack);
+        }
+        return true;
+    }
+
+    protected String getPath(byte[] key){
+        return pathStorage.get(key).poll();
+    }
+
+    //So generate keypair
+    //Maybe Message Authentication Code? Yes. To tell if message is corrupted or altered.
+    // Also maybe randomize whether is encrypted at different levels. decreased overhead
+    // So Hmac must be passed with the message on the way back.
+    // Have to read more.
+
 }
 
